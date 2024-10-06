bc6fd5659746d50348e38f67937c46628ba99bb2
jhonatan diaz <diazjhonatan1221@gmail.com>
2024-10-06 17:41:59 +1100

    se crear interfaz de error de pagina

---
+++ src/404.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./output.css" />
+    <link href="./output.css" rel="stylesheet" />
+    
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
+      crossorigin="anonymous"
+    />
+    <title>Error 404</title>
+</head>
+<body>
+<main class="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
+  <div class="text-center">
+    <p class="text-base font-semibold text-indigo-600" style="font-size: 60px;">404</p>
+    <h1 class="mt-4 text-3x2 font-bold tracking-tight text-gray-900 sm:text-5x2" style="font-size: 80px;">Page not found</h1>
+    <p class="mt-6 text-base leading-7 text-gray-600" style="font-size: 30px;">Sorry, we couldn’t find the page you’re looking for.</p>
+
+</div>
+</main>
+</body>
+</html>
